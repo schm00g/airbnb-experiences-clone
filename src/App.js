d609b220222e740dbdb32fcec01e1fc7d38e9c4e
schm00g <hk8igureu@relay.firefox.com>
2022-01-28 15:57:12 +0000

    add unique key to child cmpnts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
   const cards = data.map(item => {
     return (
       <Card 
+        key={item.id}
         img={item.coverImg}
         rating={item.stats.rating}
         reviewCount={item.stats.reviewCount}
@@ -22,7 +23,7 @@ export default function App() {
       <Navbar/>
       <Hero />
       <section className="cards-list">
-          {cards}
+        {cards}
       </section>
     </div>
   );

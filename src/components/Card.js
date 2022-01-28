b0d614d3b82257e11ed93bc38f3eb3fac6f408a9
schm00g <hk8igureu@relay.firefox.com>
2022-01-28 19:15:56 +0000

    spread props down to cmpnt

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Star from "../images/star.png";
 
-export default function Card({img, rating, reviewCount, country, title, price, openSpots}){
+export default function Card(props){
+    let badgeText;
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT";
+    } else if (props.location === "Online"){
+        badgeText = "ONLINE";
+    }
     return (
         <section className="card">
-            {openSpots === 0 &&
-                <div className="card--badge">SOLD OUT</div>
+            {badgeText &&
+                <div className="card--badge">{badgeText}</div>
             }
-            <img className="card--image" src={require(`../images/${img}`)} alt="card"/>
-            <p><img className="rating--icon" src={Star} alt="star"/>{rating} <span className="gray">({reviewCount}) - {country}</span></p>
-            <p>{title}</p>
-            <p><b>From ${price}</b> / person</p>
+            <img className="card--image" src={require(`../images/${props.coverImg}`)} alt="card"/>
+            <p><img className="rating--icon" src={Star} alt="star"/>{props.rating} <span className="gray">({props.reviewCount}) - {props.country}</span></p>
+            <p>{props.title}</p>
+            <p><b>From ${props.price}</b> / person</p>
         </section>
     )
 }

096eeb50d09c377b84456bac9e0196085bc5218a
schm00g <hk8igureu@relay.firefox.com>
2022-01-28 17:00:33 +0000

    card images

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import Star from "../images/star.png";
 export default function Card({img, rating, reviewCount, country, title, price}){
     return (
         <section className="card">
-            <img className="card--image" src={`../images/${img}`} alt="swimmer"/>
+            <img className="card--image" src={require(`../images/${img}`)} alt="card"/>
             <p><img className="rating--icon" src={Star} alt="star"/>{rating} <span className="gray">({reviewCount}) - {country}</span></p>
             <p>{title}</p>
             <p><b>From ${price}</b> / person</p>
